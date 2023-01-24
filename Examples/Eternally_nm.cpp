20e9aed0970ed9083c1a07637c84c62ea7c7339e
Federico Settimo <fede.settimo@gmail.com>
2023-01-24 15:35:49 +0200

    Eternally-nM, repeating the process 3 times

--- Examples/Eternally_nm.cpp
+++ Examples/Eternally_nm.cpp
@@ -4,8 +4,6 @@ using namespace std;
 using namespace arma;
 
 // Parameters
-double tmax = 10, dt = 0.01;
-int N_ensemble = 10000;
 double gamma1 (double t) {return 1.;}
 double gamma2 (double t) {return 1.;}
 double gamma3 (double t) {return -.5*tanh(t);}
@@ -16,43 +14,54 @@ double observable (const cx_mat &rho) {
   return real(rho(0,1));
 }
 
+complex<double> I(0,1), one(1,0);
+cx_mat sigma_z = {{one,0},{0,-one}}, Id = {{one,0},{0,one}}, sigma_x = {{0,one},{one,0}}, sigma_y = {{0,-I},{I,0}};
+
 // Free-evolution effective Hamiltonian
 cx_mat H (double t) {
-  complex<double> I(0,1), one(1,0);
-  cx_mat sigma_z = {{one,0},{0,-one}}, Id = {{one,0},{0,one}};
   return -.5*b(t)*sigma_z - Id*I*Gamma(t)*0.5;
 }
 
 // J_t(rho)
 cx_mat J (const cx_mat &rho, double t) {
-  complex<double> I(0,1), one(1,0);
-  cx_mat sigma_z = {{one,0},{0,-one}}, Id = {{one,0},{0,one}}, sigma_x = {{0,one},{one,0}}, sigma_y = {{0,-I},{I,0}};
   return 0.5*(gamma1(t)*sigma_x*rho*sigma_x + gamma2(t)*sigma_y*rho*sigma_y + gamma3(t)*sigma_z*rho*sigma_z);
 }
 
 // Gamma(t)
 cx_mat Gamma (double t) {
-  complex<double> one(1,0);
-  cx_mat Id = {{one,0},{0,one}};
   return .5*gamma_sum(t)*Id;
 }
 
 // C(t)
 cx_mat C (const cx_mat &rho, double t) {
-  complex<double> one(1,0);
-  cx_mat Id = {{one,0},{0,one}};
   return 0.5*gamma_sum(t)*Id;
 }
 
 int main() {
-  roqj jump(N_ensemble, 0, tmax, dt, 1, 2);
+  double tmin = 0., tmax = 10, dt = 0.01;
+  int N_ensemble = 10000, Ncopies = 3, dimH = 2, Ntraj = 5;
+  bool printTraj = true;
+
+  roqj jump(N_ensemble, tmin, tmax, dt, Ncopies, dimH, printTraj, Ntraj);
 
   cx_vec initialState = {sin(M_PI/8.), cos(M_PI/8.)};
   jump.set_initial_state(initialState);
 
   jump.run();
 
-  jump.get_observable("average.txt");
+  jump.get_observable("average_1.txt");
+  jump.get_error_observable("error_1.txt");
+
+  jump.reset();
+  //jump.run();
+  
+  /*jump.get_observable("average_2.txt");
+  jump.get_error_observable("error_2.txt");
+
+  jump.run();
+
+  jump.get_observable("average_3.txt");
+  jump.get_error_observable("error_3.txt");*/
 
   return 0;
 }
--- Examples/plot_enM.py
+++ Examples/plot_enM.py
@@ -7,36 +7,41 @@ Nensemble = int(filein.readline())
 ti = float(filein.readline())
 tf = float (filein.readline())
 dt = float(filein.readline())
+print_traj = bool(filein.readline())
+Ntraj = int(filein.readline())
 dimH = int(filein.readline())
 Npoints = int((tf-ti)/dt)
 
 t = np.arange(ti,tf,dt)
 
-#observable = np.zeros((Nensemble, Npoints))
-#exact = np.zeros(Npoints)
+trajectories = np.zeros((Ntraj, Npoints))
+exact = np.zeros(Npoints)
 avg_obs = np.zeros(Npoints)
-#filein = open("observables.txt")
-#exact = open("analytic.txt")
+err_obs = np.zeros(Npoints)
+filein = open("trajectories.txt")
+f_exact = open("analytic.txt")
 f_avg = open("average.txt")
+f_err = open("error.txt")
 for i in range(Npoints):
-    #noJumps[i] = f_noJ.readline()
-    #exact[i] = f_exact.readline()
+    exact[i] = f_exact.readline()
     avg_obs[i] = f_avg.readline()
-    #j = 0
-    #line = filein.readline()
-    #for x in line.split():
-        #observables[j,i] = x
-        #j+=1
-#for i in range(4):
-    #plt.plot(t, observables[i,:], alpha=.1)
-#plt.plot(t,exact,color='black', label="Exact", linewidth=1)
-plt.plot(t,avg_obs,'g-', label="Average")
+    err_obs[i] = f_err.readline()
+    j = 0
+    line = filein.readline()
+    for x in line.split():
+        trajectories[j,i] = x
+        j+=1
+for i in range(Ntraj):
+    plt.plot(t, trajectories[i,:], alpha=.1)
+plt.plot(t,exact,color='black', label="Exact", linewidth=1)
+plt.errorbar(t,avg_obs,err_obs,'g-', label="Average")
+#plt.plot(t,avg_obs,'g-', label="Average")
 
 plt.legend(loc="lower right")
 plt.ylabel(r'$Re(\rho_{12})$')
 plt.xlabel(r'$t$')
 plt.title('Driven')
 
-plt.savefig("Eternally_nm.png")
+plt.savefig("Examples/Eternally_nm.png")
 
 plt.show()
