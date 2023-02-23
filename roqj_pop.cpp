4e53d6cf8a8c2c0629f3438b1f8f543a77cee33c
Federico Settimo <fede.settimo@gmail.com>
2023-02-23 11:23:28 +0200

    Added roqj populations for initially mixed states

--- roqj_pop.cpp
+++ roqj_pop.cpp
@@ -1,8 +1,8 @@
 #include "roqj_pop.h"
-
-qubit_roqj_pop::qubit_roqj_pop (int N_ensemble, double t_i, double t_f, double dt, int N_copies, bool verbose) {
+// ------------------------------------- qubit_roqj_pop -------------------------------------
+qubit_roqj_pop::qubit_roqj_pop (int N_states, double t_i, double t_f, double dt, int N_copies, bool verbose, double threshold) {
   srand(time(NULL));
-  initialize(N_ensemble, t_i, t_f, dt, N_copies, 2, false, 0, verbose);
+  initialize(N_states, t_i, t_f, dt, N_copies, 2, false, 0, verbose, threshold);
   ComplexEigenSolver<MatrixXcd> eigs;
   MatrixXcd R = J(projector(_initial_state),0.5*(_t_f-_t_i)) + 0.5*(C(projector(_initial_state), 0.5*(_t_f-_t_i))*projector(_initial_state) + projector(_initial_state)*C(projector(_initial_state), 0.5*(_t_f-_t_i)).transpose());
   eigs.compute(R);
@@ -90,7 +90,7 @@ VectorXd qubit_roqj_pop::run_single_iterations (bool verbose) const {
     MatrixXcd R = J(projector(initial_state_t),t) + 0.5*(C(projector(initial_state_t), t)*projector(initial_state_t) + projector(initial_state_t)*C(projector(initial_state_t), t).transpose());
     ComplexEigenSolver<MatrixXcd> eigs;
     eigs.compute(R);
-    VectorXcd eigval = eigs.eigenvalues();
+    Vector2cd eigval = eigs.eigenvalues();
     double lambda_1 = real(eigval[0])*_dt, lambda_2 = real(eigval[1])*_dt;
     MatrixXcd eigvec = eigs.eigenvectors();
     // Lets see if the first eigenvalue is actually _eig_1 or it is eig_2. If eig_2, swap
@@ -179,4 +179,159 @@ VectorXd qubit_roqj_pop::run_single_iterations (bool verbose) const {
     initial_state_t = initial_state_t.normalized();
   }
   return observables;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+// ------------------------------------- qubit_roqj_pop_mixed -------------------------------------
+qubit_roqj_pop_mixed::qubit_roqj_pop_mixed (int N_states, double t_i, double t_f, double dt, int N_copies, bool verbose, double threshold) {
+  _N_states = N_states;
+  _t_i = t_i;
+  _t_f = t_f;
+  _dt = dt;
+  _N_copies = N_copies;
+  _verbose = verbose;
+  _threshold = threshold;
+}
+
+// Setting the ensemble
+void qubit_roqj_pop_mixed::set_ensemble (const vector<pair<double, Vector2cd>> &ensemble) {
+  _ensemble = ensemble;
+  double sum_p = 0.;
+  for (auto & elem : _ensemble) {
+    if (elem.first < 0.)
+      elem.first = -elem.first;
+    sum_p += elem.first;
+  }
+  if (sum_p == 0.) {
+    set_ensemble();
+    return;
+  }
+  if (sum_p != 1.) {
+    for (auto & elem : _ensemble)
+      elem.first /= sum_p;
+  }
+}
+
+void qubit_roqj_pop_mixed::set_ensemble (const vector<double> &probabilities, const vector<Vector2cd> &states) {
+  if (probabilities.size() != states.size()) {
+    cerr << "Different number of probabilities and states -- EXIT\n";
+    exit(EXIT_FAILURE);
+  }
+  _ensemble.clear();
+  vector<pair<double, Vector2cd>> ens;
+  for (int i = 0; i < probabilities.size(); ++i)
+    ens.push_back(pair<double, Vector2cd>(probabilities[i], states[i]));
+  set_ensemble(ens);
+}
+
+void qubit_roqj_pop_mixed::set_ensemble () {
+  Vector2cd init_state = Vector2cd::Ones(_dim_Hilbert_space).normalized();
+  _ensemble.clear();
+  _ensemble.push_back(pair<double, Vector2cd>(1., init_state));
+}
+
+void qubit_roqj_pop_mixed::add_ensemble (const pair<double, Vector2cd> &state) {
+  pair<double, Vector2cd> ens(state);
+  if (ens.first == 0.) return;
+  if (ens.first < 0.) ens.first = -ens.first;
+  _ensemble.push_back(ens);
+
+  double normalization = 0.;
+  for (auto & i : _ensemble)
+    normalization += i.first;
+  if (normalization != 1.) {
+    for (auto & i : _ensemble)
+      i.first /= normalization;
+  }
+}
+
+void qubit_roqj_pop_mixed::add_ensemble (double prob, const Vector2cd &state) {
+  add_ensemble(pair<double, Vector2cd>(prob, state));
+}
+
+// Runs the ROQJ for each state and takes the average state. Repeats it _N_copies time
+void qubit_roqj_pop_mixed::run () {
+  _roqj.initialize(_N_states, _t_i, _t_f, _dt, 1, 2, false, 0, false, _threshold);
+  if (_verbose) {
+    _roqj.print_info();
+    print_ens();
+  }
+
+  ofstream params;
+  params.open("params.txt");
+  params << _N_copies << endl << _N_states << endl << _t_i << endl << _t_f << endl << _dt << endl << false << endl << 0 << endl << _dim_Hilbert_space;
+  params.close();
+
+  MatrixXd matrix_observables = MatrixXd::Zero(_num_timesteps, _N_copies);
+  // Cycle on the copies
+  for (int i = 0; i < _N_copies; ++i) {
+    if (_verbose)
+      cout << "Running copy " << i+1 << "/" << _N_copies << "...\n";
+    
+    // Cycle on the ensemble members'
+    VectorXd obs = VectorXd::Zero(_num_timesteps);
+    int n = 1;
+    for (auto & elem : _ensemble) {
+      _roqj.set_initial_state(elem.second);
+      if (_verbose)
+        cout << "\tEnsemble member " << n++ << "/" << _ensemble.size() << "...\n";
+      obs += elem.first * _roqj.run_single_iterations(false);
+    }
+    if (_verbose) cout << endl;
+
+    for (int j = 0; j < _num_timesteps; ++j)
+      matrix_observables(j,i) = obs(j);
+  }
+
+  for (int i = 0; i < _num_timesteps; ++i) {
+    _observable[i] = matrix_observables.row(i).mean();
+    _sigma_observable[i] = sqrt((matrix_observables.row(i).array() - _observable[i]).square().sum() / (matrix_observables.row(i).size() - 1));
+  }
+}
+
+
+VectorXd qubit_roqj_pop_mixed::get_exact_sol (string file_out) {
+  ofstream out;
+  bool verbose = file_out != "";
+  if (verbose)
+    out.open(file_out);
+
+  MatrixXcd rho = MatrixXcd::Zero(_dim_Hilbert_space, _dim_Hilbert_space);
+  for (auto & elem : _ensemble)
+    rho += elem.first*projector(elem.second);
+
+  VectorXd obs = VectorXd(_num_timesteps);
+  int n = 0;
+
+  for (double t = _t_i; t <= _t_f; t += _dt) {
+    double this_obs = observable(rho);
+    if (verbose)
+      out << this_obs << endl;
+    obs[n] = this_obs;
+    n++;
+    rho = rho + (-I*comm(H(t),rho) + J(rho,t) - 0.5*anticomm(Gamma(t),rho))*_dt;
+  }
+
+  return obs;
+}
+
+void qubit_roqj_pop_mixed::print_ens () {
+  cout << "Ensemble:\n";
+  cout << "\tState\t| Probability\n";
+  for (auto & elem : _ensemble) {
+    cout << "----------------|----------------\n";
+    cout << elem.second << "\t| " << elem.first << endl;
+  }
+  cout << endl;
+  return;
 }
--- roqj_pop.h
+++ roqj_pop.h
@@ -9,7 +9,7 @@
 
 #include "roqj.h"
 
-extern const int N_ensemble_default, N_copies_default, dim_Hilbert_space_default, N_traj_print_default;
+extern const int N_states_default, N_copies_default, dim_Hilbert_space_default, N_traj_print_default;
 extern const double t_i_default, t_f_default, dt_default, threshold_default;
 
 // ------------------------- Qubit ROQJ pop class -------------------------
@@ -19,9 +19,9 @@ protected:
 public:
   /* 
     Parameters: (int) ensemble size, (double) intial time, (double) final time, (double) dt, (int) number of copies, (bool) verbose
-    Default values: N_ensemble = 10000, t_i = 0, t_f = 10, dt = t_f/10000, N_copies = 1, print_trajectory = true, N_traj_print = 3, verbose = true
+    Default values: N_states = 10000, t_i = 0, t_f = 10, dt = t_f/10000, N_copies = 1, print_trajectory = true, N_traj_print = 3, verbose = true
   */
-  qubit_roqj_pop (int N_ensemble = N_ensemble_default, double t_i = t_i_default, double t_f = t_f_default, double dt = dt_default, int N_copies = N_copies_default, bool verbose = true);
+  qubit_roqj_pop (int N_states = N_states_default, double t_i = t_i_default, double t_f = t_f_default, double dt = dt_default, int N_copies = N_copies_default, bool verbose = true, double threshold = threshold_default);
 
   // Runs with the 2-channel jumps
   void run ();
@@ -29,6 +29,39 @@ public:
   // Single iteration with the 2-channel jumps
   VectorXd run_single_iterations (bool verbose = true) const;
 };
+
+
+// ------------------------- Qubit ROQJ pop initially mixed class -------------------------
+class qubit_roqj_pop_mixed : public qubit_roqj {
+protected:
+  vector<pair<double, Vector2cd>> _ensemble;
+  qubit_roqj_pop _roqj;
+public:
+  /* 
+    Parameters: (int) ensemble size, (double) intial time, (double) final time, (double) dt, (int) number of copies, (bool) verbose
+    Default values: N_states = 10000, t_i = 0, t_f = 10, dt = t_f/10000, N_copies = 1, print_trajectory = true, N_traj_print = 3, verbose = true
+  */
+  qubit_roqj_pop_mixed (int N_states = N_states_default, double t_i = t_i_default, double t_f = t_f_default, double dt = dt_default, int N_copies = N_copies_default, bool verbose = true, double threshold = threshold_default);
+
+  void set_ensemble (const vector<pair<double, Vector2cd>> &ensemble);
+  void set_ensemble (const vector<double> &probabilities, const vector<Vector2cd> &states);
+  void set_ensemble ();
+
+  // Printing the ensemble
+  void print_ens ();
+
+  // Ad a state to the ensemble
+  void add_ensemble (const pair<double, Vector2cd> &state);
+  void add_ensemble (double prob, const Vector2cd &state);
+
+  // Runs the ROQJ for each state and takes the average state. Repeats it _N_copies time
+  void run ();
+
+  // Returns the exact value for the observable
+  VectorXd get_exact_sol (string file_out = "");
+};
+
+
 
 
 #endif
