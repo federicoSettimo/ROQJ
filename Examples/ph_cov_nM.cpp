2f1875905cbca7bb7c7cc31d3b207d9dc3f2cedd
Federico Settimo <fede.settimo@gmail.com>
2023-03-22 11:34:24 +0200

    Fixed the non-P dynamics and updated the plots

--- Examples/ph_cov_nM.cpp
+++ Examples/ph_cov_nM.cpp
@@ -7,7 +7,7 @@ using namespace Eigen;
 
 double gamma_p (double t) {return exp(-.5*t);}
 double gamma_m (double t) {return exp(-.25*t);}
-double gamma_z (double t) {return 4.*exp(-3.*t/8.)*cos(2.*t)*.5;} // For k = 5 doesn't work, for k = 4 yes - the threshold is somewhere in between
+double gamma_z (double t) {return 4.*exp(-3.*t/8.)*cos(2.*t)*.5;}
 
 //double b (double t) {return 0.5*(1. + erf(2.*sqrt(2.)*(t-1.)));}
 double b (double t) {return 0.;}
@@ -26,23 +26,21 @@ MatrixXcd Gamma (double t) {
 
 MatrixXcd C (const MatrixXcd &rho, double t) {
   double mu, c3, a2 = real(rho(1,1)), eps = -.5*sqrt(gamma_m(t)*gamma_p(t)) - gamma_z(t);
-    mu = a2 == 0 ? sqrt(gamma_p(t)*gamma_m(t)) : 2.*gamma_z(t) + gamma_m(t)*(1-a2)/a2;
-  if (a2 == 0 && eps > 0)
-    mu += 2.*eps;
-
+  eps = eps > 0. ? eps : 0.;
+  mu = a2 == 0. ? sqrt(gamma_p(t)*gamma_m(t)) + 2.*eps : 2.*gamma_z(t) + gamma_m(t)*(1-a2)/a2;
   c3 = gamma_z(t) - mu;
   return 2.*mu*sigma_p*sigma_m + c3*id;
 }
 
 double observable (const MatrixXcd &rho) {return abs(rho(0,1));}
+//double observable (const MatrixXcd &rho) {return real((rho*sigma_z).trace());}
 
 int main () {
   double tmin = 0., tmax = 3., dt = 0.01, threshold = 1e-15;
-  int N_ensemble = 10000, Ncopies = 1, dimH = 2, Ntraj = 10;
+  int N_ensemble = 10000, Ncopies = 3, dimH = 2, Ntraj = 10;
   bool printTraj = true;
 
-  qubit_roqj jump(N_ensemble, tmin, tmax, dt, Ncopies, printTraj, Ntraj, true, threshold); // Without reverse jumps
-
+  qubit_roqj jump(N_ensemble, tmin, tmax, dt, Ncopies, printTraj, Ntraj, true, threshold);
   Vector2cd initialState;
   initialState << sin(M_PI/8.), cos(M_PI/8.);
   jump.set_initial_state(initialState);
@@ -51,6 +49,15 @@ int main () {
 
   jump.get_observable("average.txt");
   jump.get_error_observable("error.txt");
+
+  ofstream traj;
+  traj.open("det_traj.txt");
+  for (double t = tmin; t <= tmax; t += dt) {
+    traj << observable(projector(initialState)) << endl;
+    MatrixXcd K = H(t) + 0.5*(C(projector(initialState), t).imag() - complex<double>(0.,1.)*(Gamma(t) + C(projector(initialState), t).real() ) );
+    initialState -= K*initialState*complex<double>(0.,1.)*dt;
+    initialState.normalize();
+  }
 
   return 0;
 }
