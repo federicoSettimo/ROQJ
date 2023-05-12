22647fa97ae801a86e6125a6b96fd3cf737964f9
Federico Settimo <fede.settimo@gmail.com>
2023-05-12 08:22:44 +0300

    Fixed error in J (still not working)

--- Examples/3d_ph_cov.cpp
+++ Examples/3d_ph_cov.cpp
@@ -16,23 +16,24 @@ MatrixXcd H (double t) {
 }
 
 MatrixXcd J (const MatrixXcd &rho, double t) {
-  double psi0 = real(rho(0,0)), psi1 = real(rho(1,1)), psi2 = real(rho(2,2)), g = gamma_deph(t);
+  complex<double> psi0 = rho(0,0), psi1 = rho(1,1), psi2 = rho(2,2);
+  double g = gamma_deph(t);
   MatrixXcd JJ = -g*rho;
-  JJ(0,0) = g*psi0 + psi1 + psi2;
-  JJ(1,1) = psi0 + g*psi1 + psi2;
-  JJ(2,2) = psi0 + psi1 + g*psi2;
+  JJ(0,0) = 2.*g*psi0 + psi1 + psi2;
+  JJ(1,1) = psi0 + 2.*g*psi1 + psi2;
+  JJ(2,2) = psi0 + psi1 + 2.*g*psi2;
   return JJ;
 }
 
 MatrixXcd Gamma (double t) {
-  return 2.*(1. + gamma_deph(t))*MatrixXcd::Identity(3,3);
+  return .5*(1. + 2.*gamma_deph(t))*MatrixXcd::Identity(3,3);
 }
 
 MatrixXcd C (const VectorXcd &psi, double t) {
   double g = gamma_deph(t), psi02 = norm(psi(0)), psi12 = norm(psi(1)), psi22 = norm(psi(2)), c2;
   if (psi02 != 0.)
-    c2 = 3.*g + psi12/psi02 + psi22/psi02; // c2ub
-  else c2 = (g + 2.*g*psi22*psi22 + 3.*(psi22 + psi22*psi22))/(-1. - 2.*psi22*psi22 + 4.*g*psi22*(psi22-1.));//c2lb if psi02 = 0
+    c2 = 4.*g + psi12/psi02 + psi22/psi02; // c2ub
+  else c2 = (3.*(2. - 3.*psi12 + psi12*psi12) + g*(3. - 4.*psi12 + 2.*psi12*psi12))/(-3. - 4.*(g-1.)*psi12 + (4.*g - 2.)*psi12*psi12); //c2lb if psi02 = 0
 
   MatrixXcd CC = MatrixXcd::Zero(3,3);
 
