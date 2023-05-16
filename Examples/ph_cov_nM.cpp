f2f28b46bbc3f3b4821b2e065ff5af14a2746b7f
Federico Settimo <fede.settimo@gmail.com>
2023-05-16 13:45:59 +0300

    Example of pos unravelling for non-P div ph cov. Only ok for some intial states

--- Examples/ph_cov_nM.cpp
+++ Examples/ph_cov_nM.cpp
@@ -7,7 +7,7 @@ using namespace Eigen;
 
 double gamma_p (double t) {return exp(-.5*t);}
 double gamma_m (double t) {return exp(-.25*t);}
-double gamma_z (double t) {return 10*exp(-3.*t/8.)*cos(2.*t)*.5;}
+double gamma_z (double t) {return 5.*exp(-3.*t/8.)*cos(2.*t)*.5;}
 
 //double b (double t) {return 0.5*(1. + erf(2.*sqrt(2.)*(t-1.)));}
 double b (double t) {return 0.;}
@@ -24,10 +24,11 @@ MatrixXcd Gamma (double t) {
   return gamma_p(t)*sigma_m*sigma_p + gamma_m(t)*sigma_p*sigma_m + gamma_z(t)*id;
 }
 
-MatrixXcd C (const MatrixXcd &rho, double t) {
-  double mu, c3, a2 = real(rho(1,1)), eps = -.5*sqrt(gamma_m(t)*gamma_p(t)) - gamma_z(t);
+MatrixXcd C (const VectorXcd &psi, double t) {
+  double mu, c3, a2 = norm(psi(1)), eps = -.5*sqrt(gamma_m(t)*gamma_p(t)) - gamma_z(t);
   eps = eps > 0. ? eps : 0.;
   mu = a2 == 0. ? sqrt(gamma_p(t)*gamma_m(t)) + 2.*eps : 2.*gamma_z(t) + gamma_m(t)*(1-a2)/a2;
+  //mu = a2 == 1. ? -sqrt(gamma_p(t)*gamma_m(t)) - 2.*eps : -gamma_p(t)*a2/(1.-a2) + sqrt(gamma_p(t)*gamma_m(t)) + 2.*eps;
   c3 = gamma_z(t) - mu;
   return 2.*mu*sigma_p*sigma_m + c3*id;
 }
@@ -36,7 +37,7 @@ double observable (const MatrixXcd &rho) {return abs(rho(0,1));}
 //double observable (const MatrixXcd &rho) {return real((rho*sigma_z).trace());}
 
 int main () {
-  double tmin = 0., tmax = 3., dt = 0.01, threshold = 1e-15;
+  double tmin = 0., tmax = 3., dt = 0.01, threshold = 1e-10;
   int N_ensemble = 10000, Ncopies = 1, dimH = 2, Ntraj = 10;
   bool printTraj = true;
 
