7351404d5abcb4a606a3d76dc1f2e7e775e04c67
Federico Settimo <fede.settimo@gmail.com>
2023-05-30 13:32:17 +0300

    Fixed the det nM, now giving a possible threshold for R>0

--- Examples/det_nM.cpp
+++ Examples/det_nM.cpp
@@ -60,14 +60,14 @@ double get_non_Markovianity_det (double kapppa);
 double TD (const Matrix2cd &rho, const Matrix2cd &sigma);
 
 int main () {
-  double dkappa = 0.05;
+  double dkappa = 0.1;
 
   ofstream out, np;
   out.open("det_nM.txt");
   np.open("det_nM_np.txt");
 
   int npoints = 0.;
-  for (double kappa = 1.; kappa < 8.25; kappa += dkappa) {
+  for (double kappa = 0.; kappa < 8.25; kappa += dkappa) {
     cout << "kappa = " << kappa << endl;
     out << kappa << " " << get_non_Markovianity(kappa) << " " << get_non_Markovianity_det(kappa) << endl;
     npoints++;
@@ -101,101 +101,79 @@ double get_non_Markovianity (double kappa) {
   return Nmax;
 }
 
-/*
 double get_non_Markovianity_det (double kappa) {
-  double Nmax = 0.;
-  for (double theta = 0.; theta < .5*M_PI + dtheta; theta += dtheta) {
-    if (theta > .5*M_PI) theta = .5*M_PI;
-    Vector2cd psi1 = cos(theta)*ground_state + sin(theta)*excited_state, psi2 = cos(M_PI-theta)*ground_state + sin(M_PI-theta)*excited_state;
+  double Nmax = 0., theta1_max = -1., theta2_max = -1.;
 
-    double oldd = 1., newd = 1., N = 0., pdet1 = 1., pdet2 = 1.;
-    for (double t = 0.; t < tmax; t += dt) {
-      oldd = newd;
-      
-      psi1 -= .5*(Gamma(t, kappa) + C(psi1, t, kappa, theta))*psi1*dt;
-      psi2 -= .5*(Gamma(t, kappa) + C(psi2, t, kappa, M_PI-theta))*psi2*dt;
-      psi1.normalize();
-      psi2.normalize();
-
-      Matrix2cd rho1 = projector(psi1), rho2 = projector(psi2), R = J(rho1, t, kappa) + .5*(C(psi1,t,kappa,theta)*rho1 + rho1*C(psi1,t,kappa,theta).adjoint());
-      pdet1 -= dt*real(R.trace());
-      if (real(R.trace()) < 0.) break;
-      R = J(rho2, t, kappa) + .5*(C(psi2,t,kappa,M_PI-theta)*rho2 + rho2*C(psi2,t,kappa,M_PI-theta).adjoint());
-      pdet2 -= dt*real(R.trace());
-      if (real(R.trace()) < 0.) break;
-
-      newd = TD(pdet1*rho1,pdet2*rho2);
-      if (newd > oldd) N += (newd-oldd);
-    }
-
-    if (N > Nmax) Nmax = N;
-  }
-  return Nmax;
-}
-*/
-
-/*
-double get_non_Markovianity_det (double kappa) {
-  double Nmax = 0.;
-
-  for (double theta1 = 0.; theta1 < M_PI + dtheta; theta1 += dtheta) {
+  //for (double theta1 = 0.; theta1 < M_PI + dtheta; theta1 += dtheta) {
+  for (double theta1 = 0.; theta1 < 3.; theta1 += dtheta) {
+    if (theta1 > M_PI) theta1 = M_PI;
     bool ok_theta1 = true;
 
     // Cycle also on theta2. If theta1 gives some <0 rates, just skip this theta1
     for (double theta2 = 0.; theta2 < .5*M_PI && ok_theta1; theta2 += dtheta) {
       bool ok_theta2 = true;
-      Vector2cd psi1 = cos(theta1)*ground_state + sin(theta1)*excited_state, psi2 = cos(theta2)*ground_state + sin(theta2)*excited_state;
+      Vector2cd psi1 = cos(theta1)*ground_state + sin(theta1)*excited_state, psi2 = cos(theta2)*ground_state + sin(theta2)*excited_state, psi1_n, psi2_n;
       psi1.normalize();
       psi2.normalize();
 
-      double oldd = 1., newd = 1., N = 0., pdet1 = 1., pdet2 = 1.;
+      double oldd = 1., newd = 1., N = 0.;
+      // Cycle on t, but only for the initial states giving a positive unravelling at all times
       for (double t = 0.; t < tmax && ok_theta1 && ok_theta2; t += dt) {
-        // Cycle on t, but only for the initial states giving a positive unravelling at all times
         oldd = newd;
 
-        psi1 -= .5*(Gamma(t, kappa) + C(psi1, t, kappa, theta1))*psi1*dt;
-        psi2 -= .5*(Gamma(t, kappa) + C(psi2, t, kappa, theta2))*psi2*dt;
-        psi1.normalize();
-        psi2.normalize();
+        psi1 -= .5*(Gamma(t, kappa) + C(psi1.normalized(), t, kappa, theta1))*psi1*dt;
+        psi2 -= .5*(Gamma(t, kappa) + C(psi2.normalized(), t, kappa, theta2))*psi2*dt;
 
-        Matrix2cd rho1 = projector(psi1), rho2 = projector(psi2), R = J(rho1, t, kappa) + .5*(C(psi1,t,kappa,theta1)*rho1 + rho1*C(psi1,t,kappa,theta1).adjoint());
-        pdet1 -= dt*real(R.trace())*pdet1;
-        if (real(R.trace()) < 0.) ok_theta1 = false;
-        R = J(rho2, t, kappa) + .5*(C(psi2,t,kappa,theta2)*rho2 + rho2*C(psi2,t,kappa,theta2).adjoint());
-        pdet2 -= dt*real(R.trace())*pdet2;
-        if (real(R.trace()) < 0.) ok_theta2 = false;
+        psi1_n = psi1.normalized();
+        psi2_n = psi2.normalized();
+        Matrix2cd rho1 = projector(psi1_n), rho2 = projector(psi2_n), R = J(rho1, t, kappa) + .5*(C(psi1_n,t,kappa,theta1)*rho1 + rho1*C(psi1_n,t,kappa,theta1).adjoint());
+        ComplexEigenSolver<MatrixXcd> eigs;
+        eigs.compute(R);
+        VectorXcd eigval = eigs.eigenvalues();
+        if (real(R.trace()) < -1e-10 || real(eigval(0))*real(eigval(1)) < -1e-10) ok_theta1 = false;
+        R = J(rho2, t, kappa) + .5*(C(psi2_n,t,kappa,theta2)*rho2 + rho2*C(psi2_n,t,kappa,theta2).adjoint());
+        eigs.compute(R);
+        eigval = eigs.eigenvalues();
+        if (real(R.trace()) < -1e-10 || real(eigval(0))*real(eigval(1)) < -1e-10) ok_theta2 = false;
 
-        newd = TD(pdet1*rho1,pdet2*rho2);
+        //newd = TD(rho1,rho2);
+        newd = TD(projector(psi1), projector(psi2));
+        if (psi1.norm() > 1. || psi2.norm() > 1. || psi1.norm() < 0. || psi2.norm() < 0.) {
+          cout << t << " " << kappa << ", " << newd << " " << oldd << " " << psi1.norm() << " " << psi2.norm() << ", " << theta1 << " " << theta2 << endl;
+        }
         if (newd > oldd) N += (newd-oldd);
       }
-      if (N > Nmax)
+      if (N > Nmax) {
         Nmax = N;
+        theta1_max = theta1;
+        theta2_max = theta2;
+      }
     }
   }
+  cout << "Max for theta1 = " << theta1_max << ", theta2 = " << theta2_max << endl;
   return Nmax;
 }
-*/
 
+/*
 double get_non_Markovianity_det (double kappa) {
   double Nmax = 0.;
 
   for (double theta = 0.; theta < .5*M_PI; theta += dtheta) {
     bool theta_ok = true;
-    Vector2cd psi = (cos(theta)*ground_state + sin(theta)*excited_state).normalized();
+    Vector2cd psi = (cos(theta)*ground_state + sin(theta)*excited_state).normalized(), psi_n;
 
     double oldd = 1., newd = 1., N = 0., pdet = 1.;
     for (double t = 0.; t < tmax && theta_ok; t += dt) {
       psi -= .5*(Gamma(t, kappa) + C(psi, t, kappa, theta))*psi*dt;
-      psi.normalize();
+      //psi.normalize();
 
-      Matrix2cd rho = projector(psi), R = J(rho, t, kappa) + .5*(C(psi,t,kappa,theta)*rho + rho*C(psi,t,kappa,theta).adjoint());
+      psi_n = psi.normalized();
+      Matrix2cd rho = projector(psi_n), R = J(rho, t, kappa) + .5*(C(psi_n,t,kappa,theta)*rho + rho*C(psi_n,t,kappa,theta).adjoint());
 
-      double dpdet = real(R.trace());
-      if (dpdet < -1e-10) theta_ok = false;
-      pdet -= dt*dpdet*pdet;
+      if (real(R.trace()) < -1e-10) theta_ok = false;
 
       oldd = newd;
-      newd = 2.*real(projector(psi)(0,1))*pdet; // Revival in TD = revival in coherence
+      newd = 2.*real(projector(psi)(0,1)); // Revival in TD = revival in coherence
       if (newd > oldd && theta_ok) {
         //cout << t << " " << kappa << " " << theta << ", " << newd << " " << oldd << " " << newd - oldd << " " << pdet << endl;
         N += (newd - oldd);
@@ -206,6 +184,7 @@ double get_non_Markovianity_det (double kappa) {
   }
   return Nmax;
 }
+*/
 
 double TD (const Matrix2cd &rho, const Matrix2cd &sigma) {
   Matrix2cd A = rho - sigma;
